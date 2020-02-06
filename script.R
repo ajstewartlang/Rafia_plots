6a4f54df33fb28fd0dde1e2f1eb6ca3bba93e6ce
duncanbradley <47359202+duncanbradley@users.noreply.github.com>
2020-02-06 21:05:13 +0000

    Update script.R
    - y axis now consistent in all graphs
    - summary files for y axis limits
    - code for increased gridlines

--- script.R
+++ script.R
@@ -12,33 +12,47 @@ create_df <- function(my_df){
     mutate(x = as.factor(x))
 }
 
+# creating a wrapper function to wrap the title text
+wrapper <- function(x, ...) 
+{
+  paste(strwrap(x, ...), collapse = "\n")
+}
+
 # the following function takes a tibble, a label for the x-axis, a label for 
 # the y-axis, and a title and constructs a jittered graph
+# this graph is passed to 'raw'
+# the y axis limits are passed to a vector
 my_graph <- function(df, labx, laby, title) {
   set.seed(1234)
-  df %>% 
+  raw <- df %>% 
     ggplot(aes(x = x, y = y)) +
     geom_jitter(width = .1, alpha = .75, height = 0) +
     labs(x = labx, y = laby, title = title) +
-    theme(axis.title = element_text(size = 12),
-          axis.text.x = element_text( size = 10),
+    theme(text = element_text(size = 18),
           panel.grid = element_blank()) +
-    expand_limits(y = 0)
+    expand_limits(y = 0) +
+    ggtitle(wrapper(title, width = 30)) +
+    theme(panel.background = element_rect(fill = "white"))
+  
+  vec <- ggplot_build(raw)$layout$panel_params[[1]]$y.range
+  return(list(raw, vec))
+  
 }
 
 my_bar_graph <- function(df, labx, laby, title) {
   set.seed(1234)
-  df %>% 
-    group_by(x) %>%
-    summarise(mean = mean(y)) %>%
-    ggplot(aes(x = x, y = mean)) +
-    geom_col() +
+  df %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_jitter(aes(y = y), width = .1, alpha = 0, height = 0) +
+    stat_summary(geom = "col", fun.y = mean) +
     labs(x = labx, y = laby, title = title) +
-    theme(axis.title = element_text(size = 12),
-          axis.text.x = element_text( size = 10),
+    theme(text = element_text(size = 18),
           panel.grid = element_blank()) +
-    expand_limits(y = 0)
+    expand_limits(y = 0) +
+    ggtitle(wrapper(title, width = 30)) +
+    theme(panel.background = element_rect(fill = "white"))
 }
+
 
 my_blank_graph <- function(df, labx, laby, title) {
   set.seed(1234)
@@ -46,16 +60,32 @@ my_blank_graph <- function(df, labx, laby, title) {
     ggplot(aes(x = x, y = y)) +
     geom_jitter(width = .1, alpha = 0, height = 0) +
     labs(x = labx, y = laby, title = title) +
-    theme(axis.title = element_text(size = 12),
-          axis.text.x = element_text( size = 10),
-          panel.grid = element_blank())
+    theme(text = element_text(size = 18)) +
+    expand_limits(y = 0) +
+    ggtitle(wrapper(title, width = 30)) +
+    theme(panel.background = element_rect(fill = "white"),
+          panel.grid = element_line(colour = "black"))
+
+
+  # adding gridlines manually - don't use
+          #majors <- ggplot_build(p)$layout$panel_params[[1]]$y.major_source
+          #multiplier <- 4
+          #minors <- seq(from = 0,
+                        #to = ((nth(majors, 2)) + (max(majors))),
+                        #to = max(majors),
+                        #length.out = ((length(majors) - 1) * multiplier) + 1)
+          #p <- p + scale_y_continuous(minor_breaks = minors)
+          #return(list(p, majors))
+
 }
+
 
 # the following function takes a graph and saves it in the graphs folder, 
 # generates summary statistics of the data and saves them as a .csv file in the 
 # summary_stats folder. In both cases a unique index is created to allow the
 # graph and the summary stats to be paired up later durind coding of the 
 # respones of participants. 
+
 save_graph <- function(current_graph){
   ggsave(paste0("graphs/graph_scatter_", index, ".jpg"), current_graph)
   build_this_one %>%
@@ -76,6 +106,11 @@ save_bar_graph <- function(current_graph){
 
 save_blank_graph <- function(current_graph){
   ggsave(paste0("graphs/graph_blank_", index, ".jpg"), current_graph)
+  build_this_one %>%
+    group_by(x) %>%
+    summarise(mean = mean(y), sd = sd(y)) %>%
+    cbind(index) %>%
+    write_csv(paste0("summary_stats/summary_blank", index, ".csv"))
 }  
 
 # MAIN CODE ####
@@ -91,11 +126,17 @@ for(index in my_graphs$graph_id) {
     filter(graph_id == index) %>%
     create_df() 
   
-  my_graph(build_this_one, 
-           my_graphs[my_graphs$graph_id == index,]$x_label, 
-           my_graphs[my_graphs$graph_id == index,]$y_label, 
-           my_graphs[my_graphs$graph_id == index,]$title) %>%
-    save_graph()
+  raw_graph <- my_graph(build_this_one, 
+                        my_graphs[my_graphs$graph_id == index,]$x_label, 
+                        my_graphs[my_graphs$graph_id == index,]$y_label, 
+                        my_graphs[my_graphs$graph_id == index,]$title) 
+  
+  save_graph(raw_graph[[1]])
+  raw_graph[[2]] %>%
+    enframe() %>%
+    pivot_wider(names_from = name, values_from = value) %>%
+    cbind(index) %>%
+    write_csv(., paste0("summary_stats/summary", index, "axis.csv"))
 }
 
 # build bar graphs
@@ -120,10 +161,19 @@ for(index in my_graphs$graph_id) {
     create_df() 
   
   my_blank_graph(build_this_one, 
-               my_graphs[my_graphs$graph_id == index,]$x_label, 
-               my_graphs[my_graphs$graph_id == index,]$y_label, 
-               my_graphs[my_graphs$graph_id == index,]$title) %>%
+                 my_graphs[my_graphs$graph_id == index,]$x_label,
+                 my_graphs[my_graphs$graph_id == index,]$y_label, 
+                 my_graphs[my_graphs$graph_id == index,]$title) %>%
     save_blank_graph()
+  
+  # only use when plot is named with assignment operator
+  #blank_graph <- my_blank_graph(build_this_one, 
+                          #my_graphs[my_graphs$graph_id == index,]$x_label,
+                          #my_graphs[my_graphs$graph_id == index,]$y_label, 
+                          #my_graphs[my_graphs$graph_id == index,]$title)
+  
+    #save_blank_graph(blank_graph[[1]])
+  
 }
 
 # I HAVEN'T YET RUN THIS
@@ -140,4 +190,13 @@ my_data <- files %>%
   select(- index)
 
 write_csv(my_data, file.path(data_path, "all_summary.csv"))
+
+# the same for the files containing the y axis limits
+axisfiles <- dir(data_path, pattern = "*axis.csv") # get file names
+
+axis_data <- axisfiles %>%
+  map(function(x) read_csv(file.path(data_path, x))) %>%  
+  reduce(rbind) %>%
+  mutate(graph_id = index) %>%
+  select(- index)
 
