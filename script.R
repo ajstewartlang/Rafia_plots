7448e5b76484f6c60134b3cf4af35f01647de1a7
ajstewartlang <andrew.stewart@manchester.ac.uk>
2020-02-06 21:49:10 +0000

    update

--- script.R
+++ script.R
@@ -132,6 +132,7 @@ for(index in my_graphs$graph_id) {
                         my_graphs[my_graphs$graph_id == index,]$title) 
   
   save_graph(raw_graph[[1]])
+  
   raw_graph[[2]] %>%
     enframe() %>%
     pivot_wider(names_from = name, values_from = value) %>%
